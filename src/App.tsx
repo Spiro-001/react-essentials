15fe9d5664af99ed0feebd3a2551f3ab183d5c50
Yong Kim <yhkim4573@gmail.com>
2023-03-29 19:51:11 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BasicButton from './react-essentials/Buttons/SimpleButtons/BasicButton';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicButton />
+    </div>
+  );
+}
+
+export default App;
