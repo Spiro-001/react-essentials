265a3a0b5b125b85e26cb66b0d8cb02947e9d038
Yong Kim <yhkim4573@gmail.com>
2023-03-30 01:07:03 -0400

    BasicButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
-import BasicButton from './react-essentials/Buttons/SimpleButtons/BasicButton';
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+import "./App.css";
+import { BasicButton } from "./react-essentials/Buttons/SimpleButtons/BasicButton";
 
 function App() {
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+  const [counter, setCounter] = useState<boolean>(false);
+
+  const buttonClick = () => {
+    setCounter((prev) => {
+      return !prev;
+    });
+  };
+
   return (
     <div className="App">
-      <BasicButton />
+      <BasicButton
+        bStyle={{ backgroundColor: "red" }}
+        onClick={buttonClick}
+        value="Register"
+        aSettings={{ rotation: 360, duration: 1 }}
+        ref={buttonRef}
+      />
     </div>
   );
 }
--- src/react-essentials/Buttons/SimpleButtons/BasicButton.tsx
+++ src/react-essentials/Buttons/SimpleButtons/BasicButton.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { forwardRef, MouseEvent } from "react";
+import defaultButtonClick from "./mouseclick.mp3";
+import "./BasicButton.css";
 
-export default function BasicButton () {
-    return <div>
-        Ww
-    </div>
-}
+type BasicButtonProps = {
+  bStyle?: Record<string, string>;
+  onClick(): void;
+  value?: string;
+  aSettings?: Record<string, any>;
+  children?: React.ReactNode;
+};
+
+export const BasicButton = forwardRef<HTMLDivElement, BasicButtonProps>(
+  function BasicButton(
+    { bStyle, onClick, value, aSettings, children }: BasicButtonProps,
+    ref
+  ) {
+    const bClickAnim = (element: MouseEvent<HTMLDivElement>) => {
+      return onClick();
+    };
+
+    return (
+      <div
+        style={bStyle}
+        onClick={bClickAnim}
+        ref={ref}
+        className="basic-button"
+      >
+        {value ? value : children}
+      </div>
+    );
+  }
+);
