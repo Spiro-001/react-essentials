ee71c8a6079c2f0b14438d6ad8f73cdc7323345c
Yong Kim <yhkim4573@gmail.com>
2023-04-04 13:48:07 -0400

    BasicList gsap delete item anim

--- src/react-essentials/Lists/BasicList/BasicList.tsx
+++ src/react-essentials/Lists/BasicList/BasicList.tsx
@@ -14,6 +14,11 @@ type BasicListProp = {
   children?: React.ReactNode;
 };
 
+type listElementProp = {
+  idx: number;
+  order: string;
+};
+
 export const BasicList = ({
   bStyle,
   onClick,
@@ -23,68 +28,73 @@ export const BasicList = ({
 }: BasicListProp) => {
   const [listObjects, setListObjects] =
     useState<Record<number, string>>(listObjectsProp);
-  const [forceReload, setForceReload] = useState<number>(0);
 
   const bListRef = useRef<Record<string, HTMLSpanElement | null>>({});
   const noListRef = useRef<HTMLDivElement | null>(null);
   const listObjectRef = useRef<Record<number, string>>(listObjects);
+
+  const defaultStyle = {
+    minHeight: "300px",
+    minWidth: "200px",
+  };
+
+  const defaultStyleNoItems = {
+    minHeight: "300px",
+    minWidth: "200px",
+  };
 
   const lClick = (element: React.MouseEvent<HTMLSpanElement>) => {
     gsap.to(element.target, {
       opacity: 0,
       height: 0,
       padding: "0px 24px",
-      duration: 1,
-      onComplete: () => {
-        onClick(element, listObjects, setListObjects);
-      },
+      duration: 0.3,
     });
+    onClick(element, listObjects, setListObjects);
   };
 
   useEffect(() => {
-    listObjectRef.current = listObjects;
-    console.log(listObjectRef.current, listObjects);
     if (Object.keys(listObjects).length === 0) {
       const tl = gsap.to(noListRef.current, {
         opacity: 0,
         height: 0,
-        duration: 0.2,
-        padding: 0,
+        padding: "0px 24px",
+        duration: 0.3,
+        display: "none",
       });
       tl.progress(1);
       tl.reverse();
     }
-  }, [listObjects, forceReload]);
+  }, [listObjects]);
 
-  if (Object.keys(listObjectRef.current).length !== 0 || children) {
+  const listElement = (idx: number, order: string) => {
+    console.log(listObjects);
     return (
-      <div style={bStyle} className="basic-list">
+      <span
+        onClick={lClick}
+        className="list-item"
+        key={idx}
+        id={order}
+        ref={(ref) => (bListRef.current[order] = ref)}
+      >
+        {listObjects[parseInt(order)]}
+      </span>
+    );
+  };
+
+  if (Object.keys(listObjects).length !== 0 || children) {
+    return (
+      <div style={bStyle ? bStyle : defaultStyle} className="basic-list">
         {children
           ? children
           : Object.keys(listObjectRef.current).map((order, idx) => {
-              if (
-                Object.keys(listObjects).length <
-                Object.keys(listObjectRef.current).length
-              ) {
-                // setForceReload((prev) => prev + 1);
-              }
-              return (
-                <span
-                  onClick={lClick}
-                  className="list-item"
-                  key={idx}
-                  id={order}
-                  ref={(ref) => (bListRef.current[order] = ref)}
-                >
-                  {listObjects[parseInt(order)]}
-                </span>
-              );
+              return listElement(idx, order);
             })}
       </div>
     );
   }
   return (
-    <div style={bStyle} className="basic-list">
+    <div style={bStyle ? bStyle : defaultStyleNoItems} className="basic-list">
       <span className="list-item" ref={noListRef}>
         {ifEmpty}
       </span>
